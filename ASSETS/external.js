33f31b269f50a3d685b29f13193354defe60466b
Eshan Trivedi <eshan.trivedi.9@gmail.com>
2022-10-23 19:31:26 +0530

    refactored the code

--- ASSETS/external.js
+++ ASSETS/external.js
@@ -1,17 +1,9 @@
-  //adding toggle button for light and dark mode 
-  const toggleButton = document.getElementById("toggle-btn");
-  toggleButton.onclick = function () {
-    document.body.classList.toggle("light-theme");
-    
-  }
-
-  const copyButton = document.querySelectorAll(".copy-btn");
+  var copyButton = document.querySelectorAll(".copy-btn");
   copyButton.forEach((btn) => {
     btn.addEventListener("click", () => {
       var code = btn.previousElementSibling.innerText;
       code = code.slice(2, code.length);
       navigator.clipboard.writeText(code);
-      // Adding Alert When text is copied and removing after 800ms
       btn.classList.add("copied")
       setTimeout(function () {
         btn.classList.remove("copied")
@@ -20,9 +12,9 @@
   });
 
   function search() {
-    let input = document.getElementById('searchbar').value
+    var input = document.getElementById('searchbar').value
     input = input.toLowerCase();
-    let x = document.getElementsByClassName('searchItem');
+    var x = document.getElementsByClassName('searchItem');
 
     for (i = 0; i < x.length; i++) {
       if (!x[i].innerHTML.toLowerCase().includes(input)) {
@@ -34,10 +26,8 @@
     }
   }
 
-  // Get the button:
-let mybutton = document.getElementById("myBtn");
+var mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -48,7 +38,6 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
@@ -61,6 +50,4 @@ function myFunction() {
   } else {
     x.style.display = "block";
   }
-
-
 }
