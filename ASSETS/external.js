5da353ccafc0133a62f9731e64cd696ac02be3ba
KCmean <kunalmanu2468@gmail.com>
2022-10-28 19:46:50 +0530

    accept kar le bhai

--- ASSETS/external.js
+++ ASSETS/external.js
@@ -28,10 +28,6 @@ function scrollFunction() {
     mybutton.style.display = "none";
   }
 }
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
 function myFunction() {
   var x = document.getElementById("myLinks");
   if (x.style.display === "block") {
